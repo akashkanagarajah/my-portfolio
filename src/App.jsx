7baab2e8f5ac162a62626f0782deedb0a6ac5c41
akashkanagarajah <akashkanagarajah@gmail.com>
2025-07-07 14:55:08 -0400

    Initial commit: Vite + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <h1 className="text-4xl font-bold text-emerald-400 underline">
+        Tailwind is working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
